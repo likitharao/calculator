dcf3923d49d2388bf005fcb3a95d3075f5a40da2
Likitha Rao <37280873+likitharao@users.noreply.github.com>
2025-05-16 19:37:53 +0530

    Add files via upload

---
+++ calculator project/app.js
@@ -0,0 +1,40 @@
+
+let input = document.getElementById('input');
+let buttons = document.querySelectorAll('button');
+let string = "";
+let arr = Array.from(buttons);
+
+arr.forEach(button => 
+    {
+  button.addEventListener('click', (e) =>
+     {
+    let value = e.target.innerHTML;
+    if (value === '=') 
+        {
+      try 
+      {
+        string = eval(string);
+      } 
+      catch 
+      {
+        string = "Error";
+      }
+      input.value = string;
+    } 
+    else if (value === 'AC')
+         {
+      string = "";
+      input.value = string;
+    } 
+    else if (value === 'DEL')
+         {
+      string = string.slice(0, -1);
+      input.value = string;
+    } 
+    else 
+    {
+      string += value;
+      input.value = string;
+    }
+  });
+});
